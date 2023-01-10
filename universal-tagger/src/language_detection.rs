3a756280f90d6144ef580f42a5bb17e2bc0eac29
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2023-01-10 16:33:45 -0300

    feat(universal-tagger)!: support allow and deny

--- universal-tagger/src/language_detection.rs
+++ universal-tagger/src/language_detection.rs
@@ -5,7 +5,7 @@ use strum::EnumIter;
 /// Language codes following the [ISO 639-3](https://en.wikipedia.org/wiki/ISO_639-3) standard.
 #[non_exhaustive]
 #[derive(Clone, Copy, Debug, EnumIter, PartialEq, Eq, PartialOrd, Ord)]
-enum Language {
+enum Lang {
     /// العربية (Arabic).
     Ara,
     /// বাংলা (Bengali).
@@ -42,52 +42,52 @@ enum Language {
     Urd,
 }
 
-impl From<Language> for whatlang::Lang {
-    fn from(lang: Language) -> Self {
+impl From<Lang> for whatlang::Lang {
+    fn from(lang: Lang) -> Self {
         match lang {
-            Language::Ara => whatlang::Lang::Ara,
-            Language::Ben => whatlang::Lang::Ben,
-            Language::Cmn => whatlang::Lang::Cmn,
-            Language::Deu => whatlang::Lang::Deu,
-            Language::Eng => whatlang::Lang::Eng,
-            Language::Epo => whatlang::Lang::Epo,
-            Language::Fra => whatlang::Lang::Fra,
-            Language::Hin => whatlang::Lang::Hin,
-            Language::Ind => whatlang::Lang::Ind,
-            Language::Ita => whatlang::Lang::Ita,
-            Language::Jpn => whatlang::Lang::Jpn,
-            Language::Pan => whatlang::Lang::Pan,
-            Language::Por => whatlang::Lang::Por,
-            Language::Rus => whatlang::Lang::Rus,
-            Language::Spa => whatlang::Lang::Spa,
-            Language::Tur => whatlang::Lang::Tur,
-            Language::Urd => whatlang::Lang::Urd,
+            Lang::Ara => whatlang::Lang::Ara,
+            Lang::Ben => whatlang::Lang::Ben,
+            Lang::Cmn => whatlang::Lang::Cmn,
+            Lang::Deu => whatlang::Lang::Deu,
+            Lang::Eng => whatlang::Lang::Eng,
+            Lang::Epo => whatlang::Lang::Epo,
+            Lang::Fra => whatlang::Lang::Fra,
+            Lang::Hin => whatlang::Lang::Hin,
+            Lang::Ind => whatlang::Lang::Ind,
+            Lang::Ita => whatlang::Lang::Ita,
+            Lang::Jpn => whatlang::Lang::Jpn,
+            Lang::Pan => whatlang::Lang::Pan,
+            Lang::Por => whatlang::Lang::Por,
+            Lang::Rus => whatlang::Lang::Rus,
+            Lang::Spa => whatlang::Lang::Spa,
+            Lang::Tur => whatlang::Lang::Tur,
+            Lang::Urd => whatlang::Lang::Urd,
         }
     }
 }
 
-impl TryFrom<whatlang::Lang> for Language {
+impl TryFrom<whatlang::Lang> for Lang {
     type Error = whatlang::Lang;
 
     fn try_from(lang: whatlang::Lang) -> Result<Self, Self::Error> {
         match lang {
-            whatlang::Lang::Ara => Ok(Language::Ara),
-            whatlang::Lang::Ben => Ok(Language::Ben),
-            whatlang::Lang::Cmn => Ok(Language::Cmn),
-            whatlang::Lang::Deu => Ok(Language::Deu),
-            whatlang::Lang::Eng => Ok(Language::Eng),
-            whatlang::Lang::Epo => Ok(Language::Epo),
-            whatlang::Lang::Fra => Ok(Language::Fra),
-            whatlang::Lang::Hin => Ok(Language::Hin),
-            whatlang::Lang::Ind => Ok(Language::Ind),
-            whatlang::Lang::Ita => Ok(Language::Ita),
-            whatlang::Lang::Jpn => Ok(Language::Jpn),
-            whatlang::Lang::Pan => Ok(Language::Pan),
-            whatlang::Lang::Por => Ok(Language::Por),
-            whatlang::Lang::Rus => Ok(Language::Rus),
-            whatlang::Lang::Spa => Ok(Language::Spa),
-            whatlang::Lang::Tur => Ok(Language::Tur),
-            whatlang::Lang::Urd => Ok(Language::Urd),
+            whatlang::Lang::Ara => Ok(Lang::Ara),
+            whatlang::Lang::Ben => Ok(Lang::Ben),
+            whatlang::Lang::Cmn => Ok(Lang::Cmn),
+            whatlang::Lang::Deu => Ok(Lang::Deu),
+            whatlang::Lang::Eng => Ok(Lang::Eng),
+            whatlang::Lang::Epo => Ok(Lang::Epo),
+            whatlang::Lang::Fra => Ok(Lang::Fra),
+            whatlang::Lang::Hin => Ok(Lang::Hin),
+            whatlang::Lang::Ind => Ok(Lang::Ind),
+            whatlang::Lang::Ita => Ok(Lang::Ita),
+            whatlang::Lang::Jpn => Ok(Lang::Jpn),
+            whatlang::Lang::Pan => Ok(Lang::Pan),
+            whatlang::Lang::Por => Ok(Lang::Por),
+            whatlang::Lang::Rus => Ok(Lang::Rus),
+            whatlang::Lang::Spa => Ok(Lang::Spa),
+            whatlang::Lang::Tur => Ok(Lang::Tur),
+            whatlang::Lang::Urd => Ok(Lang::Urd),
             lang => Err(lang),
         }
     }
@@ -95,25 +95,45 @@ impl TryFrom<whatlang::Lang> for Language {
 
 /// Language detector.
 struct Detector {
-    languages: BTreeSet<Language>,
+    langs: BTreeSet<Lang>,
 }
 
 impl Default for Detector {
     fn default() -> Self {
-        use strum::IntoEnumIterator;
-
-        let languages = Language::iter().collect();
-        Self { languages }
+        Self::all()
     }
 }
 
 impl Detector {
+    fn empty() -> Self {
+        Self {
+            langs: BTreeSet::default(),
+        }
+    }
+
+    fn all() -> Self {
+        use strum::IntoEnumIterator;
+
+        let langs = Lang::iter().collect();
+        Self { langs }
+    }
+
+    fn allow(&mut self, lang: Lang) -> &mut Self {
+        self.langs.insert(lang);
+        self
+    }
+
+    fn deny(&mut self, lang: Lang) -> &mut Self {
+        self.langs.remove(&lang);
+        self
+    }
+
     /// Detect a natural language.
     ///
     /// This returns [`None`] whenever the detection fails, its result
     /// is unreliable or it is probably a language we don't support at the moment.
-    fn detect(&self, text: &str) -> Option<Language> {
-        let allowlist = self.languages.iter().map(|&lang| lang.into()).collect();
+    fn detect(&self, text: &str) -> Option<Lang> {
+        let allowlist = self.langs.iter().map(|&lang| lang.into()).collect();
         let detector = whatlang::Detector::with_allowlist(allowlist);
 
         let info = detector.detect(text)?;
@@ -133,18 +153,22 @@ mod tests {
     fn it_works() {
         let text = "Ĉu vi ne volas eklerni Esperanton? Bonvolu! Estas unu de la plej bonaj aferoj!";
         let lang = Detector::default().detect(text).unwrap();
-        assert_eq!(lang, Language::Epo);
+        assert_eq!(lang, Lang::Epo);
 
         let text = "There is no reason not to learn Esperanto.";
-        let lang = Detector::default().detect(text).unwrap();
-        assert_eq!(lang, Language::Eng);
+        let lang = Detector::empty()
+            .allow(Lang::Eng)
+            .allow(Lang::Rus)
+            .detect(text)
+            .unwrap();
+        assert_eq!(lang, Lang::Eng);
     }
 
     #[test]
     fn conversion_roundtrip() {
         use strum::IntoEnumIterator;
 
-        for lang in Language::iter() {
+        for lang in Lang::iter() {
             assert_eq!(Ok(lang), whatlang::Lang::from(lang).try_into());
         }
     }
